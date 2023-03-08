a69d1c00fd9d6838e8e2325edc389b6cfbb3ed17
Mohit Nandwani <mohitnandwani@me.com>
2023-03-08 18:05:20 +0530

    fixing spectrum view UI

--- Sources/ColorSpectrumPicker.swift
+++ Sources/ColorSpectrumPicker.swift
@@ -89,7 +89,8 @@ class ColorSpectrumPicker: UIView, UIGestureRecognizerDelegate {
         
         DispatchQueue.main.async { [self] in
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseInOut) {
-                let xPos = hue * self.bounds.width //self.pickerOverlayView.center.x
+                let xPos = hue * self.bounds.width
+                print("xPos:", xPos, self.pickerOverlayView.center.x)
                 let yPos = hue * self.bounds.height
                 self.pickerOverlayView.center = CGPoint(x: xPos, y: yPos)
             }

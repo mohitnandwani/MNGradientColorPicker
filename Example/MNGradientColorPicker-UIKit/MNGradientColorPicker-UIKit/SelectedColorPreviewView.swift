5463bc7df01e0035ac04b34e31ccf1cae3744850
Mohit Nandwani <mohitnandwani@me.com>
2023-03-08 17:18:33 +0530

    Minor Fixes for example

---
+++ Example/MNGradientColorPicker-UIKit/MNGradientColorPicker-UIKit/SelectedColorPreviewView.swift
@@ -0,0 +1,16 @@
+
+//
+//  SelectedColorPreview.swift
+//  MNGradientColorPicker-UIKit
+//
+//  Created by Mohit Nandwani on 29/10/22.
+//
+import UIKit
+
+class SelectedColorPreview: UIView {
+    
+    override class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+    
+}
